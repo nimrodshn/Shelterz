540659a293f1d9ac3b607f505f10ef2344eb2869
nimrodshn <nshneor@redhat.com>
2017-06-02 17:14:25 +0300

    working on posting local adresses to server

--- app.js
+++ app.js
@@ -4,32 +4,39 @@ var path = require('path');
 var fs = require('fs');
 var obj = 0;
 
+var options = {
+  host: 'maps.googleapis.com',
+  path: '/maps/api/directions/json?origin=' + '' + '&destination=' + '' + 'l&avoid=highways&mode=bicycling'
+}
+
 function parseShelterzDB(){
   // Parsing JSON.
-  fs.readFileSync('db/kml.geojson', 'utf8', function(err, data){
+  fs.readFileSync('db/kml.json', 'utf8', function(err, data){
     if (err) throw err();
     else{
       // Parse default list of shelterz
       obj = JSON.parse(data);
-      console.log(obj);
-      console.log("lfksjdlkfjds");
     }
   });
 }
 
 app.get('/', function (req, res) {
-  // req params are latitude and longitude
-  // compute the shortest distance using Google Maps API and report back the results.
-  // should probably show the actoual route using google maps api.
-  parseShelterzDB();
   res.sendFile(path.join(
     __dirname, 'client', 'index.html'));
 });
 
+app.post('/', function(req, res){
+  // post current location for calcualtions agains db list.
+  // req params are latitude and longitude
+  // compute the shortest distance using Google Maps API and report back the results.
+  // should probably show the actoual route using google maps api.
+
+});
 
 var server = app.listen(8080, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
-
+   // parse default db once upon init.
+   parseShelterzDB();
 });
