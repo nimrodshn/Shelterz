7c166f656735832f1a1eeb8862d5246152a22791
nimrodshn <nshneor@redhat.com>
2017-07-28 16:03:08 +0300

    begining db stuff

--- models/database.js
+++ models/database.js
@@ -1,8 +1,10 @@
 const pg = require('pg');
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/todo';
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/shelterz';
 
 const client = new pg.Client(connectionString);
 client.connect();
-const query = client.query(
-  'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+
+// initiate db table
+
+const query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, lat INTEGER not null, lon INTEGER not null)');
 query.on('end', () => { client.end(); });
